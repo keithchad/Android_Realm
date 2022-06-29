9b99c0d4f715d93880fb1ba26019115fc46fc2ba
keithchad <keithchad10@outlook.com>
2022-06-29 13:30:06 +0300

    view validation

--- activities/MainActivity.kt
+++ activities/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.chad.gads2022_java_kotlin.R
 import io.realm.RealmConfiguration
 import android.content.Intent
+import android.widget.EditText
 import com.chad.gads2022_java_kotlin.app.Constants
+import com.google.android.material.textfield.TextInputLayout
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,39 +20,66 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize Realm
         initializeRealm()
+
+        //Views
         initialize()
+
     }
 
     //Initialize Views
     private fun initialize() {
         setSupportActionBar(toolbar_main)
 
+        //Save On Click Listener
         saveNameButton.setOnClickListener {
 
         }
 
+        //Repositories On Click Listener
         listRepositoriesButton.setOnClickListener {
-            val repoName = etRepoName.text.toString()
-            val language = etLanguage.text.toString()
+            if(isNotEmpty(etRepoName, inputLayoutName)) {
 
-            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
-            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
-            intent.putExtra(Constants.KEY_REPO_SEARCH, repoName)
-            intent.putExtra(Constants.KEY_LANGUAGE, language)
-            startActivity(intent)
+                val repoName = etRepoName.text.toString()
+                val language = etLanguage.text.toString()
+
+                val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+                intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+                intent.putExtra(Constants.KEY_REPO_SEARCH, repoName)
+                intent.putExtra(Constants.KEY_LANGUAGE, language)
+                startActivity(intent)
+
+            }
 
         }
 
+        //User Repositories On Click Listener
         listUserRepositoriesButton.setOnClickListener {
-            val githubUser = etGithubUser.text.toString()
 
-            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
-            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
-            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
-            startActivity(intent)
+            if(isNotEmpty(etGithubUser, inputLayoutGithubUser)) {
+                val githubUser = etGithubUser.text.toString()
+
+                val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+                intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+                intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+                startActivity(intent)
+            }
+
         }
 
+    }
+
+    //Validation
+    private fun isNotEmpty(edittext: EditText, textInputLayout: TextInputLayout) : Boolean {
+        if(edittext.text.toString().isEmpty()) {
+            textInputLayout.error = "Blank"
+            return false
+        } else {
+            textInputLayout.isErrorEnabled = false
+            return true
+        }
     }
 
     //Initialize Realm
