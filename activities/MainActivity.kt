c062a572f9679c7f42fb4d33cfc5b4d19b9d5d07
keithchad <keithchad10@outlook.com>
2022-06-29 12:00:28 +0300

    kotlin extensions

--- activities/MainActivity.kt
+++ activities/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.chad.gads2022_java_kotlin.activities
 
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.EditText
-import com.google.android.material.textfield.TextInputLayout
 import android.os.Bundle
 import com.chad.gads2022_java_kotlin.R
 import io.realm.RealmConfiguration
-import android.content.SharedPreferences
 import android.content.Intent
-import android.view.View
-import com.chad.gads2022_java_kotlin.activities.DisplayActivity
 import com.chad.gads2022_java_kotlin.app.Constants
 import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var etName: EditText? = null
-    private var etGithubRepoName: EditText? = null
-    private var etLanguage: EditText? = null
-    private var etGithubUser: EditText? = null
-    private var inputLayoutName: TextInputLayout? = null
-    private var inputLayoutRepoName: TextInputLayout? = null
-    private var inputLayoutGithubUser: TextInputLayout? = null
+
+    companion object {
+        private val TAG: String = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,13 +24,33 @@ class MainActivity : AppCompatActivity() {
 
     //Initialize Views
     private fun initialize() {
-        etName = findViewById(R.id.etName)
-        etGithubRepoName = findViewById(R.id.etRepoName)
-        etLanguage = findViewById(R.id.etLanguage)
-        etGithubUser = findViewById(R.id.etGithubUser)
-        inputLayoutName = findViewById(R.id.inputLayoutName)
-        inputLayoutRepoName = findViewById(R.id.inputLayoutRepoName)
-        inputLayoutGithubUser = findViewById(R.id.inputLayoutGithubUser)
+        setSupportActionBar(toolbar_main)
+
+        saveNameButton.setOnClickListener {
+
+        }
+
+        listRepositoriesButton.setOnClickListener {
+            val repoName = etRepoName.text.toString()
+            val language = etLanguage.text.toString()
+
+            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+            intent.putExtra(Constants.KEY_REPO_SEARCH, repoName)
+            intent.putExtra(Constants.KEY_LANGUAGE, language)
+            startActivity(intent)
+
+        }
+
+        listUserRepositoriesButton.setOnClickListener {
+            val githubUser = etGithubUser.text.toString()
+
+            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+            startActivity(intent)
+        }
+
     }
 
     //Initialize Realm
@@ -49,49 +63,5 @@ class MainActivity : AppCompatActivity() {
         Realm.setDefaultConfiguration(config)
     }
 
-    // Save app username in SharedPreferences
-    fun saveName(view: View?) {
-        if (isNotEmpty(etName, inputLayoutName)) {
-            val personName = etName!!.text.toString()
-            val sp = getSharedPreferences(Constants.APP_SHARED_PREFERENCES, MODE_PRIVATE)
-            val editor = sp.edit()
-            editor.putString(Constants.KEY_PERSON_NAME, personName)
-            editor.apply()
-        }
-    }
 
-    // Search repositories on github
-    fun listRepositories(view: View?) {
-        if (isNotEmpty(etGithubRepoName, inputLayoutRepoName)) {
-            val queryRepo = etGithubRepoName!!.text.toString()
-            val repoLanguage = etLanguage!!.text.toString()
-            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
-            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
-            intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
-            intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
-            startActivity(intent)
-        }
-    }
-
-    //Search repositories of a particular github user
-    fun listUserRepositories(view: View?) {
-        if (isNotEmpty(etGithubUser, inputLayoutGithubUser)) {
-            val githubUser = etGithubUser!!.text.toString()
-            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
-            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
-            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
-            startActivity(intent)
-        }
-    }
-
-    //Validation
-    private fun isNotEmpty(editText: EditText?, textInputLayout: TextInputLayout?): Boolean {
-        return if (editText!!.text.toString().isEmpty()) {
-            textInputLayout!!.error = "Cannot be blank !"
-            false
-        } else {
-            textInputLayout!!.isErrorEnabled = false
-            true
-        }
-    }
 }
